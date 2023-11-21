7c6b36b07240fbb680094ac4db8910a9a42fbaeb
IntelMin <intelmin0915@gmail.com>
2023-11-21 18:01:08 -0500

    add vercel.json & error handler

--- controllers/auth-sync.controller.js
+++ controllers/auth-sync.controller.js
@@ -7,26 +7,30 @@ const {
 require('dotenv').config()
 
 export async function syncUsers(req, res) {
-  console.log("Raw User Data:", req.body);
-
-  let error;
-  switch (req.body.type) {
-    case 'user.created':
-      error = await insertUser(req.body.data);
-      break;
-    case 'user.updated':
-      error = await updateUser(req.body.data);
-      break;
-    case 'user.deleted':
-      error = await deleteUser(req.body.data);
-      break;
-    default:
-      res.status(422).json({message: "Invalid Type"});
-      return;
+  try {
+    console.log("[syncUsers] Raw User Data:", req.body);
+  
+    let error;
+    switch (req.body.type) {
+      case 'user.created':
+        error = await insertUser(req.body.data);
+        break;
+      case 'user.updated':
+        error = await updateUser(req.body.data);
+        break;
+      case 'user.deleted':
+        error = await deleteUser(req.body.data);
+        break;
+      default:
+        res.status(422).json({message: "Invalid Type"});
+        return;
+    }
+  
+    if (error) 
+      res.status(500).json({code: error.code, message: error.message});
+    else
+      res.status(200).json({message: "DB Operation Success"});
+  } catch(e) {
+    console.log("[syncUsers] Error:", e);
   }
-
-  if (error) 
-    res.status(500).json({code: error.code, message: error.message});
-  else
-    res.status(200).json({message: "DB Operation Success"});
 }
